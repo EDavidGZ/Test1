601d98e0c1863682b1a8d2800359d56457a21b80
EDavidGZ <jgar05202415@gmail.com>
2022-12-02 21:59:46 -0600

    Initial commit

---
+++ src/Test/Test.java
@@ -0,0 +1,19 @@
+package Test;
+
+
+
+public class Test {
+
+    public static void main(String[] args) {
+        int count = 0;
+        long start = 0b11011110;
+        char caracter = 'b';
+        double decimal = 2.3d;
+        float flotante = 0.2f;
+        boolean verdadero = false;
+        String name = "Efren";
+
+        System.out.println("Esto es lo que pidio? " + count + " " + start + " " + caracter + " " + decimal + flotante + verdadero + name );
+
+    }
+}
